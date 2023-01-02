6d586ce0f718a4ee7d4f8ceae3cf76fbb651dff7
gaudhiwaa <gaudhiwaa@gmail.com>
2023-01-02 08:48:55 +0700

    feat: navigation bar

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,30 +1,52 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import LogoDark from '@/data/agoraDarkMode.png'
+import LogoLight from '@/data/agoraLightMode.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+import { useState } from 'react'
 
 const LayoutWrapper = ({ children }) => {
+  // const [theme, setTheme] = useState("light")
+  const { theme, setTheme, resolvedTheme } = useTheme()
   return (
     <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
+      <div className="-mt-14 flex h-screen flex-col justify-between">
         <header className="flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
+              <div className="flex items-center justify-between ">
+                <div className="mr-3 ">
+                  {/* <Logo /> */}
+                  {theme === 'dark' ? (
+                    <Image
+                      alt={'Agora Indonesia'}
+                      src={LogoLight}
+                      // className="object-cover object-center md:h-36 lg:h-48"
+                      width={125}
+                      height={125}
+                    />
+                  ) : (
+                    <Image
+                      alt={'Agora Indonesia'}
+                      src={LogoDark}
+                      // className="object-cover object-center md:h-36 lg:h-48"
+                      width={125}
+                      height={125}
+                    />
+                  )}
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
+                    {/* {siteMetadata.headerTitle} */}
                   </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+                ) : // siteMetadata.headerTitle
+                null}
               </div>
             </Link>
           </div>
@@ -34,7 +56,7 @@ const LayoutWrapper = ({ children }) => {
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4 "
                 >
                   {link.title}
                 </Link>
